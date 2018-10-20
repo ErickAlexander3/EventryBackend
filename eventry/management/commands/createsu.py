d2e2c5b33b1e8e1a50bc26104a25a12643ff90a5
Erick Velasquez <erickalexander_03@hotmail.com>
2018-10-20 14:23:50 -0700

    added the command for admin user creation and started the placeholder events app

---
+++ eventry/management/commands/createsu.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="erickadmin").exists():
+            User.objects.create_superuser("erickadmin", "erick@admin.com", "admin")
+
+        if not User.objects.filter(username="nilooadmin").exists():
+            User.objects.create_superuser("nilooadmin", "niloo@admin.com", "admin")
+
+        if not User.objects.filter(username="nancyadmin").exists():
+            User.objects.create_superuser("nancyadmin", "nancy@admin.com", "admin")
+
+        if not User.objects.filter(username="graceadmin").exists():
+            User.objects.create_superuser("graceadmin", "grace@admin.com", "admin")
